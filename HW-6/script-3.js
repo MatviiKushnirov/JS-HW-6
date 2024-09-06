015b993c44cf744bd1cd37bbd086122fd1d0fe06
Matvii <mkushnirov@gmail.com>
2024-09-06 23:18:04 +0300

    first commit

---
+++ HW-6/script-3.js
@@ -0,0 +1,10 @@
+// Task 3
+// Вивести на екран таблицю множення числа 7 за допомогою циклу for.
+
+let maxNum = 70;
+let count = 1;
+
+for(let i = 7; i <= maxNum;i+=7){
+    console.log(`7 * ${count} = ${i}`);
+    count++;
+}
